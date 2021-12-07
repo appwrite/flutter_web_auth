cc2c2158d8a7b46756d3555f5fa71476178ac2ae
Silfalion <na_rutoshippuden@hotmail.fr>
2021-12-07 08:43:17 +0100

    Update flutter_web_auth.dart

--- lib/flutter_web_auth.dart
+++ lib/flutter_web_auth.dart
@@ -1,49 +1,56 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:html';
+import 'dart:js';
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart' show MethodChannel;
+import 'package:flutter/services.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-class _OnAppLifecycleResumeObserver extends WidgetsBindingObserver {
-  final Function onResumed;
+class FlutterWebAuthPlugin {
+  static void registerWith(Registrar registrar) {
+    final MethodChannel channel = MethodChannel(
+        'flutter_web_auth', const StandardMethodCodec(), registrar.messenger);
+    final FlutterWebAuthPlugin instance = FlutterWebAuthPlugin();
+    channel.setMethodCallHandler(instance.handleMethodCall);
+  }
 
-  _OnAppLifecycleResumeObserver(this.onResumed);
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      onResumed();
+  Future<dynamic> handleMethodCall(MethodCall call) async {
+    switch (call.method) {
+      case 'authenticate':
+        final String url = call.arguments['url'];
+        return _authenticate(url);
+      default:
+        throw PlatformException(
+            code: 'Unimplemented',
+            details: "The flutter_web_auth plugin for web doesn't implement "
+                "the method '${call.method}'");
     }
   }
-}
 
-class FlutterWebAuth {
-  static const MethodChannel _channel = const MethodChannel('flutter_web_auth');
-
-  static final _OnAppLifecycleResumeObserver _resumedObserver = _OnAppLifecycleResumeObserver(() {
-    _cleanUpDanglingCalls(); // unawaited
-  });
-
-  /// Ask the user to authenticate to the specified web service.
-  ///
-  /// The page pointed to by [url] will be loaded and displayed to the user. From the page, the user can authenticate herself and grant access to the app. On completion, the service will send a callback URL with an authentication token, and this URL will be result of the returned [Future].
-  ///
-  /// [callbackUrlScheme] should be a string specifying the scheme of the url that the page will redirect to upon successful authentication.
-  /// [preferEphemeral] if this is specified as `true`, an ephemeral web browser session will be used where possible (`FLAG_ACTIVITY_NO_HISTORY` on Android, `prefersEphemeralWebBrowserSession` on iOS/macOS)
-  static Future<String> authenticate({required String url, required String callbackUrlScheme, bool? preferEphemeral}) async {
-    WidgetsBinding.instance?.removeObserver(_resumedObserver); // safety measure so we never add this observer twice
-    WidgetsBinding.instance?.addObserver(_resumedObserver);
-    return await _channel.invokeMethod('authenticate', <String, dynamic>{
-      'url': url,
-      'callbackUrlScheme': callbackUrlScheme,
-      'preferEphemeral': preferEphemeral ?? false,
-    }) as String;
-  }
-
-  /// On Android, the plugin has to store the Result callbacks in order to pass the result back to the caller of
-  /// `authenticate`. But if that result never comes the callback will dangle around forever. This can be called to
-  /// terminate all `authenticate` calls with an error.
-  static Future<void> _cleanUpDanglingCalls() async {
-    await _channel.invokeMethod('cleanUpDanglingCalls');
-    WidgetsBinding.instance?.removeObserver(_resumedObserver);
+  static Future<String> _authenticate(String url) async {
+    context.callMethod('open', [url]);
+    await for (MessageEvent messageEvent in window.onMessage) {
+      if (messageEvent.origin == Uri.base.origin) {
+        final flutterWebAuthMessage = messageEvent.data['flutter-web-auth'];
+        if (flutterWebAuthMessage is String) {
+          return flutterWebAuthMessage;
+        }
+      }
+      var appleOrigin = Uri(scheme: 'https', host: 'appleid.apple.com');
+      if (messageEvent.origin == appleOrigin.toString()) {
+        try {
+          Map<String, dynamic> data = jsonDecode(messageEvent.data);
+          if (data['method'] == 'oauthDone') {
+            final appleAuth = data['data']['authorization'];
+            if (appleAuth != null) {
+              final appleAuthQuery = Uri(queryParameters: appleAuth).query;
+              return appleOrigin.replace(fragment: appleAuthQuery).toString();
+            }
+          }
+        } on FormatException {}
+      }
+    }
+    throw new PlatformException(
+        code: 'error', message: 'Iterable window.onMessage is empty');
   }
 }
